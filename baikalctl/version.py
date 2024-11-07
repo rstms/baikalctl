127e20fd03db2909a145d744bc71ff2123a51e25
Matt Krueger <mkrueger@rstms.net>
2024-11-07 00:54:23 -0700

    Bump version: 0.0.2 → 0.0.3

--- baikalctl/version.py
+++ baikalctl/version.py
@@ -1,4 +1,4 @@
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 __author__ = "Matt Krueger"
 __email__ = "mkrueger@rstms.net"
-__timestamp__ = "2024-11-07 00:53:52-07:00"
+__timestamp__ = "2024-11-07 00:54:22-07:00"
